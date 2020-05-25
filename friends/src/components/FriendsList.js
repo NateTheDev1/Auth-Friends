bf2d3791842829676879168dc062777f3b7349df
NateTheDev1 <nthnlrichards@gmail.com>
2020-05-25 18:25:50 -0400

    MVP Done

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -3,11 +3,13 @@ import { axiosWithAuth } from "../axiosWithAuth";
 
 const FriendsList = () => {
   const [friends, setFriends] = useState([]);
+  const [newFriend, setNewFriend] = useState({});
 
   useEffect(() => {
     axiosWithAuth()
       .get("http://localhost:5000/api/friends")
       .then((res) => {
+        console.log(res.data);
         setFriends(res.data);
       })
       .catch((err) => {
@@ -15,8 +17,53 @@ const FriendsList = () => {
       });
   }, []);
 
+  const addFriend = (e) => {
+    e.preventDefault();
+    console.log(newFriend);
+    axiosWithAuth()
+      .post("http://localhost:5000/api/friends", newFriend)
+      .then((res) => {
+        console.log(res.data);
+        setFriends(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleFormChange = (e) => {
+    setNewFriend({
+      ...newFriend,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div>
+      <form onSubmit={addFriend}>
+        <h1>Add User</h1>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={newFriend.name}
+          onChange={handleFormChange}
+        />
+        <input
+          type="number"
+          placeholder="Age"
+          name="age"
+          value={newFriend.age}
+          onChange={handleFormChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={newFriend.email}
+          onChange={handleFormChange}
+        />
+        <button type="submit">Add</button>
+      </form>
       <h1>Friends</h1>
       {friends.length !== 0 ? (
         <div>
